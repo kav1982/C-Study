1c46680668728fc135b81e86d95f37fc0e48f072
红色篝火 <48897680+kav1982@users.noreply.github.com>
2021-03-07 23:21:39 +0800

    3.7 飞船游戏起步

---
+++ Assets/Space SHMUP Prototype/__Scripts/Hero.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    static public Hero S;
+
+    [Header("Set in Inspector")]
+    //控制飞船的运动
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+
+    void Awake()
+    {
+        if (S == null)
+        {
+            S = this; //设置单例对象
+        }
+        else
+        {
+            Debug.LogError("Hero.Awake()-Attempted to assign second Hero.S!");
+        }
+    }
+
+    void Update()
+    {
+        //从用户输入中获取信息
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        //让飞船旋转一个角度,更有动感
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+    }
+    
+}
---
+++ Assets/Space SHMUP Prototype/__Scripts/Shield.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public float rotationsPerSecond = 0.1f;
+    [Header("Set Dynamically")]
+    public int levelShown = 0;
+
+    //非共有变量不会出现在检视器中
+    Material mat;
+
+    void Start()
+    {
+        int currLevel = Mathf.FloorToInt(Hero.S.shieldLevel);
+        //如果当前护盾等级与显示的等级不符
+        if (levelShown != currLevel)
+        {
+            levelShown = currLevel;
+        }
+    }
+
+}
